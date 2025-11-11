ffe54a6377fe620df935530a1d4fdefc7b1f93c6
rip_bleid <jostinmestre2007@gmail.com>
2025-11-11 15:27:03 -0500

    Inicio del proyecto

---
+++ Server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import juegosRoutes from "./routes/juegos.js";
+import reseñasRoutes from "./routes/reseñas.js";
+
+dotenv.config();
+const app = express(); 
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/juegos", juegosRoutes)
+app.use("/api/reseñas", reseñasRoutes)
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log("Conectado a MongoDB"))
+    .catch(err => console.log(err)); 
+
+app.listen(4000, () => console.log("Servidor corriendo en el puerto 4000"));
